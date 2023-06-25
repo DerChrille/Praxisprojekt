c367308ecb66fa6833164e183e979032b3312e20
DerChrille <christian.reclik@live.de>
2023-06-25 18:11:57 +0200

    Add new range picker

--- src/routes/generator/RoomAmountPicker.svelte
+++ src/routes/generator/RoomAmountPicker.svelte
@@ -1,5 +1,8 @@
 <script>
+	import { browser } from '$app/environment';
 	import { roomAmount, generatorProgress } from '$lib/stores';
+	import { onMount } from 'svelte';
+	import RangeSlider from 'svelte-range-slider-pips';
 
 	$: buttonLabel = !touchedSlider
 		? 'How many rooms?'
@@ -7,7 +10,7 @@
 	let buttonDisabed = true;
 
 	const roomMin = 1;
-	const roomMax = 5;
+	const roomMax = 4;
 
 	// set initial value
 	$roomAmount = Math.min(roomMax / 2);
@@ -19,12 +22,18 @@
 		buttonDisabed = undefined;
 	}
 
-	let sliderElement;
+	onMount(() => {
+		if (browser) {
+			let rangeEl = document.querySelector('#range-rooms');
+			rangeEl.querySelector('.pip.first .pipVal-suffix').textContent = ' room';
+		}
+	});
 </script>
 
 <h2>How long do you want to play?</h2>
 <p>Choose the amount of puzzles you want to solve.</p>
 
+<!--
 <div class="slider" data-min={roomMin} data-max={roomMax}>
 	<input
 		bind:this={sliderElement}
@@ -36,24 +45,29 @@
 		on:input={() => roomAmountChosen(sliderElement.value)}
 	/>
 </div>
+-->
+
+<RangeSlider
+	values={[$roomAmount]}
+	min={roomMin}
+	max={roomMax}
+	pipstep={roomMin}
+	all="label"
+	suffix=" rooms"
+	pips
+	on:change={(ev) => roomAmountChosen(ev.detail.value)}
+	id="range-rooms"
+/>
 
 <button disabled={buttonDisabed} on:click={() => $generatorProgress++}>{buttonLabel}</button>
 
 <style>
-	.slider {
-		width: 100%;
-		display: flex;
-		align-items: center;
-	}
-	.slider:before {
-		content: attr(data-min);
-		margin-right: 1em;
-		font-size: 140%;
-	}
-	.slider:after {
-		content: attr(data-max);
-		margin-left: 1em;
-		font-size: 140%;
+	:root {
+		--range-handle: var(--color-accent-darker);
+		--range-handle-focus: var(--color-accent);
+		--range-slider: var(--color-bg-hover);
+		--range-pip: var(--color-accent-brighter);
+		--range-pip-active: var(--color-white);
 	}
 
 	button {
